5ca46fbc93005710e52b7078cbc5f96de576a5fa
Ixtiyor <ixtiyorsaitov21@gmail.com>
2025-08-04 20:50:46 +0500

    dropdown menu

--- app/dashboard/genres/_components/index.tsx
+++ app/dashboard/genres/_components/index.tsx
@@ -1,6 +1,20 @@
 import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuTrigger,
+  DropdownMenuContent,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuItem,
+} from "@/components/ui/dropdown-menu";
 import { IGenre } from "@/types";
-import { MoreHorizontal, MoreVertical } from "lucide-react";
+import {
+  Film,
+  MoreHorizontal,
+  MoreVertical,
+  Settings,
+  Trash2,
+} from "lucide-react";
 import React from "react";
 
 const GenresPageMain = ({ datas }: { datas: IGenre[] }) => {
@@ -12,18 +26,41 @@ const GenresPageMain = ({ datas }: { datas: IGenre[] }) => {
           className="w-full px-3 py-1 flex items-center justify-between border-y"
         >
           <div className="flex items-center justify-start gap-2">
-            <Button size={"icon"} variant={'secondary'} className="size-7 rounded-full">
+            <Button
+              size={"icon"}
+              variant={"secondary"}
+              className="size-7 rounded-full"
+            >
               {index}
             </Button>
             <p>{data.name}</p>
           </div>
-          <Button
-            size={"icon"}
-            className="size-7 rounded-full"
-            variant={"ghost"}
-          >
-            <MoreVertical />
-          </Button>
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button
+                size={"icon"}
+                className="size-7 rounded-full"
+                variant={"ghost"}
+              >
+                <MoreVertical />
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+              <DropdownMenuItem>
+                <Settings />
+                Update
+              </DropdownMenuItem>
+              <DropdownMenuItem>
+                <Film />
+                Films
+              </DropdownMenuItem>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem variant="destructive">
+                <Trash2 />
+                Delete
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
         </div>
       ))}
     </div>
